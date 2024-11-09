ae84cfc39e9c4ed53c014174fa020dd51df0ce45
thithihlaing23 <thiritth23@gmail.com>
2024-11-09 22:03:44 +0630

    feat: Applying Validation

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,11 +1,18 @@
 import { FormEvent, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
 const Form = () => {
-  const { register , handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
   const onSubmit = (data: FieldValues) => console.log(data);
 
-  
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
@@ -13,12 +20,21 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register('name')}
-         
+          {...register("name" ,{ required: true, minLength: 3})}
           id="name"
           type="text"
           className="w-32 border-2"
         />
+        {errors.name?.type === "required" && (
+            <p className="text-red-500">
+              The name field is required.
+            </p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-red-500">
+            The name must be at least 3 character.
+          </p>
+        )}
       </div>
 
       <div className="mb-3">
@@ -26,12 +42,17 @@ const Form = () => {
           Age
         </label>
         <input
-         {...register('age')}
-        
+          {...register("age" ,{required:true})}
           id="age"
           type="number"
           className="w-32 border-2"
         />
+        {errors.name?.type === "required" && (
+            <p className="text-red-500">
+              The name field is required.
+            </p>
+        )}
+        
       </div>
 
       <button className="px-5 py-1 bg-blue-500 rounded-lg" type="submit">
