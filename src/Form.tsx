fbf618ea6ca64244ba58a42542ebfe404bd62841
thithihlaing23 <thiritth23@gmail.com>
2024-11-09 21:27:38 +0630

    feat: Managing Forms with React Hook Form

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,34 +1,44 @@
-import React, { FormEvent, useRef, useState } from 'react'
+import { FormEvent, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
+  const { register , handleSubmit } = useForm();
+  const onSubmit = (data: FieldValues) => console.log(data);
 
-   const[person,setPerson] = useState({
-    name:'',
-    age:0
-   });
-
-
-    const handleSubmit = (event: FormEvent) =>{
-        event.preventDefault();
-       
-        console.log(person);
-}
+  
   return (
-    <form onSubmit={handleSubmit}>
-        <div className='mb-3'>
-        <label htmlFor="name" className='mr-2'>Name</label>
-        <input onChange={(event) => setPerson({ ...person, name:event.target.value})} id='name' type="text" className='w-32 border-2' />
-        </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="mr-2">
+          Name
+        </label>
+        <input
+          {...register('name')}
+         
+          id="name"
+          type="text"
+          className="w-32 border-2"
+        />
+      </div>
 
-        <div className='mb-3'>
-        <label htmlFor="age" className='mr-2'>Age</label>
-        <input onChange={(event) => setPerson({ ...person, age: parseInt(  event.target.value)})} id='age' type="number" className='w-32 border-2' />
-        </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="mr-2">
+          Age
+        </label>
+        <input
+         {...register('age')}
+        
+          id="age"
+          type="number"
+          className="w-32 border-2"
+        />
+      </div>
 
-        <button className='px-5 py-1 bg-blue-500 rounded-lg' type='submit'>Submit</button>
-       
+      <button className="px-5 py-1 bg-blue-500 rounded-lg" type="submit">
+        Submit
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
